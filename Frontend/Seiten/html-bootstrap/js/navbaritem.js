d68525b8c7c98cc7dd21c13c19dee24d6534cc75
GitMichaGit <mbeyer98@yahoo.com>
2023-07-07 12:46:57 +0200

    fix js main pages fix js item pages fix navbar main pages fix navbar item pages - in progress

---
+++ Frontend/Seiten/html-bootstrap/js/navbaritem.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("#replace_with_navbar").load("/Frontend/Seiten/html-bootstrap/navitem.html");    
+    $("#replace_with_footer").load("/Frontend/Seiten/html-bootstrap/navitem.html");
+});
--- Frontend/Seiten/html-bootstrap/userView.js
+++ Frontend/Seiten/html-bootstrap/userView.js
@@ -1,55 +1,30 @@
-function userAdminFunction() {
-    if (localStorage.getItem('user.type') === 'admin') {
-        var cssCode = ".bi-person { display:none; }" + ".usertxt {display: none;}";
-        var styleElement = document.createElement('style');
-        styleElement.type = 'text/css';
+
+function updateUserView() {
+    var userType = localStorage.getItem('user.type');
+    var cssCode = '';
+
+    if (userType === 'admin') {
+        cssCode = ".bi-person { display:none; }" + ".usertxt {display: none;}";
         console.log("Login - Admin");
-        if (styleElement.styleSheet) {
-            // Für den Internet Explorer
-            styleElement.styleSheet.cssText = cssCode;
-        } else {
-            styleElement.appendChild(document.createTextNode(cssCode));
-        }
-
-        document.head.appendChild(styleElement);
-
-    }
-}
-
-function userNullFunction() {
-    if (localStorage.getItem('user.type') === null) {
-        var cssCode = ".bi-bag-dash { display:none; }" +
-            ".bi-box-arrow-in-right { display: none; }" + ".admintxt { display: none;}" + ".usertxt {display: none;}";
-        var styleElement = document.createElement('style');
-        styleElement.type = 'text/css';
-        console.log("Not logged in");
-        if (styleElement.styleSheet) {
-            styleElement.styleSheet.cssText = cssCode;
-        } else {
-            styleElement.appendChild(document.createTextNode(cssCode));
-        }
-
-        document.head.appendChild(styleElement);
-
-    }
-}
-
-function userUserFunction() {
-    if (localStorage.getItem('user.type') === 'user') {
-        var cssCode = ".bi-person { display:none; }" + ".admintxt { display: none;}";
-        var styleElement = document.createElement('style');
-        styleElement.type = 'text/css';
+    } else if (userType === 'user') {
+        cssCode = ".bi-person { display:none; }" + ".admintxt { display: none;}";
         console.log("Login - User");
-        if (styleElement.styleSheet) {
-            // Für den Internet Explorer
-            styleElement.styleSheet.cssText = cssCode;
-        } else {
-            styleElement.appendChild(document.createTextNode(cssCode));
-        }
-
-        document.head.appendChild(styleElement);
-
+    } else {
+        cssCode = ".bi-bag-dash { display:none; }" +
+            ".bi-box-arrow-in-right { display: none; }" + ".admintxt { display: none;}" + ".usertxt {display: none;}";
+        console.log("Not logged in");
     }
+
+    var styleElement = document.createElement('style');
+    styleElement.type = 'text/css';
+
+    if (styleElement.styleSheet) {
+        styleElement.styleSheet.cssText = cssCode;
+    } else {
+        styleElement.appendChild(document.createTextNode(cssCode));
+    }
+
+    document.head.appendChild(styleElement);
 }
 
 function logoutStorage() {
@@ -58,24 +33,101 @@ function logoutStorage() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // setTimeout wird verwendet, um eine Verzögerung zu erzeugen und sicherzustellen, dass das <li>-Element vollständig geladen ist
     setTimeout(function() {
-        // <li>-Element mit der Klasse 'nav-item' auswählen
         var navItem = document.querySelector('.nav-item');
-
-        // Überprüfen, ob das <li>-Element gefunden wurde
         if (navItem !== null) {
-            // <i>-Element mit der Klasse 'bi-box-arrow-in-right' im <li>-Element finden
             var logoutButton = navItem.querySelector('.bi-box-arrow-in-right');
-            
-            // Überprüfen, ob das <i>-Element gefunden wurde
             if (logoutButton !== null) {
-                // Klick-Ereignis dem Button hinzufügen
                 logoutButton.addEventListener('click', function () {
                     logoutStorage();
-                    // location.reload();
                 });
             }
         }
     }, 100);
+    
+    updateUserView();
 });
+
+
+// function userAdminFunction() {
+//     if (localStorage.getItem('user.type') === 'admin') {
+//         var cssCode = ".bi-person { display:none; }" + ".usertxt {display: none;}";
+//         var styleElement = document.createElement('style');
+//         styleElement.type = 'text/css';
+//         console.log("Login - Admin");
+//         if (styleElement.styleSheet) {
+//             // Für den Internet Explorer
+//             styleElement.styleSheet.cssText = cssCode;
+//         } else {
+//             styleElement.appendChild(document.createTextNode(cssCode));
+//         }
+
+//         document.head.appendChild(styleElement);
+
+//     }
+// }
+
+// function userNullFunction() {
+//     if (localStorage.getItem('user.type') === null) {
+//         var cssCode = ".bi-bag-dash { display:none; }" +
+//             ".bi-box-arrow-in-right { display: none; }" + ".admintxt { display: none;}" + ".usertxt {display: none;}";
+//         var styleElement = document.createElement('style');
+//         styleElement.type = 'text/css';
+//         console.log("Not logged in");
+//         if (styleElement.styleSheet) {
+//             styleElement.styleSheet.cssText = cssCode;
+//         } else {
+//             styleElement.appendChild(document.createTextNode(cssCode));
+//         }
+
+//         document.head.appendChild(styleElement);
+
+//     }
+// }
+
+// function userUserFunction() {
+//     if (localStorage.getItem('user.type') === 'user') {
+//         var cssCode = ".bi-person { display:none; }" + ".admintxt { display: none;}";
+//         var styleElement = document.createElement('style');
+//         styleElement.type = 'text/css';
+//         console.log("Login - User");
+//         if (styleElement.styleSheet) {
+//             // Für den Internet Explorer
+//             styleElement.styleSheet.cssText = cssCode;
+//         } else {
+//             styleElement.appendChild(document.createTextNode(cssCode));
+//         }
+
+//         document.head.appendChild(styleElement);
+
+//     }
+// }
+
+// function logoutStorage() {
+//     localStorage.clear();
+//     console.log("localStorage - flushed");
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // setTimeout wird verwendet, um eine Verzögerung zu erzeugen und sicherzustellen, dass das <li>-Element vollständig geladen ist
+//     setTimeout(function() {
+//         // <li>-Element mit der Klasse 'nav-item' auswählen
+//         var navItem = document.querySelector('.nav-item');
+
+//         // Überprüfen, ob das <li>-Element gefunden wurde
+//         if (navItem !== null) {
+//             // <i>-Element mit der Klasse 'bi-box-arrow-in-right' im <li>-Element finden
+//             var logoutButton = navItem.querySelector('.bi-box-arrow-in-right');
+        
+//             // Überprüfen, ob das <i>-Element gefunden wurde
+//             if (logoutButton !== null) {
+//                 // Klick-Ereignis dem Button hinzufügen
+//                 logoutButton.addEventListener('click', function () {
+//                     logoutStorage();
+//                     // location.reload();
+//                 });
+//             }
+//         }
+//     }, 100);
+// });
+
