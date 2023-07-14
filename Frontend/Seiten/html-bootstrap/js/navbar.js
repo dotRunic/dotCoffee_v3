d936e8a76b4c96216960420b37668fb413a50fd6
GitMichaGit <mbeyer98@yahoo.com>
2023-07-14 14:26:56 +0200

    navbar fix, icons bugged

--- Frontend/Seiten/html-bootstrap/js/navbar.js
+++ Frontend/Seiten/html-bootstrap/js/navbar.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#replace_with_navbar").load("/Frontend/Seiten/html-bootstrap/nav.html");
+    // $("#replace_with_navbar").load("/Frontend/Seiten/html-bootstrap/nav.html");
     $("#replace_with_footer").load("/Frontend/Seiten/html-bootstrap/nav.html");
 });
   
