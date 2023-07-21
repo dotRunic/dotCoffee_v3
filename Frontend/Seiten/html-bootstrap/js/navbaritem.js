f38b1afe9d9d7b6502d12d5ff3202228d054ca47
GitMichaGit <mbeyer98@yahoo.com>
2023-07-21 11:58:02 +0200

    navbar icon fix

--- Frontend/Seiten/html-bootstrap/js/navbaritem.js
+++ Frontend/Seiten/html-bootstrap/js/navbaritem.js
@@ -1,4 +1,3 @@
 $(document).ready(function() {
-    $("#replace_with_navbar").load("/Frontend/Seiten/html-bootstrap/navitem.html");    
     $("#replace_with_footer").load("/Frontend/Seiten/html-bootstrap/navitem.html");
 });
